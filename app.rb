4e7ee7893df41304ae8c4a2f73fe36dfe80da635
makarov <sdf.brain@gmail.com>
2016-06-23 13:48:51 +0300

    add comments

--- app.rb
+++ app.rb
@@ -9,11 +9,15 @@ def init_db
 end
 
 before do
+	# инициализирует базу данных
 	init_db
 end
-
+# вызывается каждый раз при инициализации приложения:
+# когда изменился код и перезагрулилась страница приложения
 configure do
+	# инициализация базы данных
 	init_db
+	# создает таблицу, если таблица не существует
 	@db.execute 'CREATE TABLE IF NOT EXISTS Posts 
 	(
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,6 +35,7 @@ get '/new' do
 end
 
 post '/new' do
+	# получаем переменную из POST-запроса
 	content = params[:content]
  	erb "#{content}"
 end

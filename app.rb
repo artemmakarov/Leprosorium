57db8bc324f68b2f4d4b8828f7192f32586bffd1
makarov <sdf.brain@gmail.com>
2016-06-23 12:47:29 +0300

    init db,make @db

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db
+end
 
 get '/' do
 	erb "Cap for /"
